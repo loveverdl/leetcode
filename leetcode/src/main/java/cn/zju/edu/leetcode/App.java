994eb080297023ce13ecd1c66f9c134583751c8e
loveverdl <loveverdl@gmail.com>
2013-07-18 13:54:39 +0800

    init

---
+++ leetcode/src/main/java/cn/zju/edu/leetcode/App.java
@@ -0,0 +1,13 @@
+package cn.zju.edu.leetcode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
